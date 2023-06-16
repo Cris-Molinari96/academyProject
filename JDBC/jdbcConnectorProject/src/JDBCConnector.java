ca26f2921194077fc1c8e15c20a510a3f52bece7
Cris <cris.molinari96@gmail.com>
2023-06-16 13:58:29 +0200

    add package db connection

--- JDBC/jdbcConnectorProject/src/JDBCConnector.java
+++ JDBC/jdbcConnectorProject/src/JDBCConnector.java
@@ -5,37 +5,48 @@ public class JDBCConnector {
     private static final String user = "root";
     private static final String pass = "root";
 
-// 1) quindi possiamo verificare se la classe driver è nota al progetto
     Connection connection = null;
     Statement statement = null;
+
+    ResultSet resultSet = null;
     public void getConnectionGiuseppe(){
         try {
-            Class.forName("com.mysql.cj.jdbc.Driver");
+
+            // check Classe Driver vista nel prog.
+            Class.forName("com.mysql.cj.jdbc.Driver"); // si può fare un check sulla cartella Driver per vedere se è implementata e vista
             System.out.println("Class founded");
 
-            connection = DriverManager.getConnection(url,user,pass); // --> restituisce un oggetto di tipo connection
-            System.out.println("DriverManager riuscito");
-
-            statement = connection.createStatement();
-
-            ResultSet resultSet = statement.executeQuery("select * from actor");
+            // Verifica connection
             try {
+                connection = DriverManager.getConnection(url, user, pass); // --> restituisce un oggetto di tipo connection
+                System.out.println("DriverManager riuscito");
+            }catch (SQLException e) {
+                System.out.println("Error connection");
+                throw new RuntimeException(e);
+            }
+
+            // Statement check
+            try {
+                statement = connection.createStatement();
+            } catch (SQLException s){
+                System.out.println("Statement error");
+            }
+
+            // Result set check
+            try{
+                ResultSet resultSet = statement.executeQuery("select * from actor");
                 while (resultSet.next()) { // questo resultSet ha un metodo il quale se trova un record da true andando avanti, mentre se false s'interrompe
                     String name = resultSet.getString("first_name");
                     String surname = resultSet.getString("last_name");
                     System.out.println(name + " " + surname);
                 }
-                connection.close();
-            }catch(SQLException s){
+            }catch (SQLException e) {
                 System.out.println("Errore di stampa");
+                throw new RuntimeException(e);
             }
 
         }catch(ClassNotFoundException c){
             System.out.println("Class not found, error!!");
-        } catch (SQLException e) {
-            System.out.println("Error connection");
-            throw new RuntimeException(e);
-
         }
     }
 
